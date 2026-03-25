145fb0193bc00a736420d1e9f46d7f184a651150
ArthurGuillemin <aguillemin291@gmail.com>
2026-03-25 19:26:09 +0100

    update submodules with esp32 latest modifications

--- UpdateSubmodule.sh
+++ UpdateSubmodule.sh
@@ -2,4 +2,4 @@
 
 cd pinball_playglass && git pull origin main && cd .. && cd pinball_backend && git pull origin main && cd .. && cd pinball_backglass && git pull origin main && cd .. && cd pinball_dmd && git pull origin main && cd .. && cd pinball_esp32 && git pull origin main && cd .. 
 
-
+git add . && git commit -m "update submodules with ... latest modifications " && git push
