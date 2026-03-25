6bf5b908a4ded302bd22061d34eb3eda04df4d50
ArthurGuillemin <aguillemin291@gmail.com>
2026-03-25 15:15:49 +0100

    Update gitsubmodules

---
+++ UpdateSubmodule.sh
@@ -0,0 +1,5 @@
+#ligne a coller pour mettre a jour les submodules d'un coup :
+
+cd pinball_playglass && git pull origin main && cd .. && cd pinball_backend && git pull origin main && cd .. && cd pinball_backglass && git pull origin main && cd .. && cd pinball_dmd && git pull origin main && cd .. && cd pinball_esp32 && git pull origin main && cd .. 
+
+
